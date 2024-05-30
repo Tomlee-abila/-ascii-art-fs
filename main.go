15443ac599082d87d41c3165d587dfb934121d9b
Tomlee Abila <tomleeabila@gmail.com>
2024-05-30 14:54:12 +0300

    handling arguments

--- main.go
+++ main.go
@@ -6,8 +6,15 @@ import (
 )
 
 func main()  {
-	if len(os.Args) != 3{
+	if len(os.Args) > 3 || len(os.Args) < 2{
 		fmt.Println("Only two arguments are accepted after main.go.\nUsage: go run . [STRING] [BANNER]\nEX: go run . something standard")
+		return
 	}
-	
+
+	BannerFile := ".txt"
+	if len(os.Args) > 2{
+		BannerFile = os.Args[2]+BannerFile
+	}else{
+		BannerFile = "standard"+BannerFile
+	}
 }
